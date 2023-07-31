6fecfc384b0b3e8c475c65e9813bd4c3be80d0d2
shoeless <ashianor.james@yahoo.com>
2023-08-01 03:23:58 +0400

    init

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// C program to illustrate macros
+
+ 
+// Macro definition
+#define AREA(l, b) (l * b)
+ 
+// Driver Code
+
+int main()
+{
+
+    // Given lengths l1 and l2
+
+    int l1 = 10, l2 = 5, area;
+ 
+
+    // Find the area using macros
+
+    area = AREA(l1, l2);
+ 
+
+    // Print the area
+
+    printf("Area of rectangle"
+
+           " is: %d",
+
+           area);
+ 
+
+    return 0;
+}
